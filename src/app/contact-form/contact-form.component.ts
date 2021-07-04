dc12ab06615dd8a4b02671ef85a80524426a204e
karolina-siemieniuk-morawska <karolinajustynska@o2.pl>
2021-07-04 16:23:00 +0200

    added validation to input fields and red border to indicate invalid inputs

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { validatePolish } from 'validate-polish';
 
 @Component({
   selector: 'app-contact-form',
@@ -15,18 +16,33 @@ export class ContactFormComponent implements OnInit {
   ngOnInit(): void {
     this.contactForm = this.formBuilder.group({
       name: ['', [Validators.required]],
-      nip: ['', [Validators.required]],
-      email: ['', [Validators.required]],
-      phone: [''],
+      nip: ['', [Validators.required, this.validateNip()]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.pattern('[- +()0-9]{9,}')]],
       topic: ['cooperation', [Validators.required]],
-      driversLicence: ['', [Validators.required]],
+      driversLicense: [''],
       message: [''],
-      agreements: ['']
+      agreements: this.formBuilder.group({
+        processing: [false, [Validators.requiredTrue]],
+        marketing: [false]
+      })
     });  
   }
-  
-  onSubmit(){
-    console.log(this.contactForm);
-  }
 
+  private validateNip(): ValidatorFn {
+      return (control: AbstractControl) => validatePolish.nip(control.value) ? null : { incorrectNip: control.value };
+  }
+  
+  public checkAll(event: any): void {
+    this.contactForm.controls.agreements.setValue({ processing: event.target.checked, marketing: event.target.checked });
+  }
+  
+  public allAgreementsSelcted(): boolean {
+    return Object.values(this.contactForm.controls.agreements.value).every(value => value === true);
+  }
+  
+  public onSubmit(): void {
+    console.log(this.contactForm.value);
+    this.contactForm.reset();
+  }
 }
